3e77422a998f3fa2e4a9332cca76c8ac8f5457df
FizaXYZ123 <Fiza@thexyzstudio.com>
2026-07-08 17:39:52 +0530

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import userRoute from "./routes/user.route.js"
+import userRoute from "./routes/user.route.js";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
+
 app.use(express.json())
+
+app.use(cors({
+origin : "http://localhost:5173",
+methods:["POST","GET","PUT","PATCH","DELETE"],
+credentials: true
+}
+));
+
 const PORT = process.env.PORT;
 const URI = process.env.MONGODB_URI
 
@@ -15,7 +25,6 @@ try {
 } catch (error) {
     console.log("error",error)
 }
-
 
 app.use("/user",userRoute);
 
